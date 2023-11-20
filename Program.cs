a98541375f9876e59956e352434e2be330d34a34
Oskar <ehrharoska@ecs.vuw.ac.nz>
2023-11-21 00:38:10 +1300

    base code

--- Program.cs
+++ Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Nikon;
+
+NikonManager manager = new NikonManager("Type0020.md3");
+
+manager.DeviceAdded += new DeviceAddedDelegate(manager_DeviceAdded);
+
+NkMAIDCapInfo[] capInfo = device.GetCapabilityInfo();
+print("Device name: " + capInfo[0].szDescription);
+
+try
+{
+    int batteryLevel = device.GetInteger(eNkMAIDCapability.kNkMAIDCapability_BatteryLevel);
+    print("Battery level: " + batteryLevel);
+}
+catch (NikonException ex)
+{
+}
+
+
+manager.Shutdown();
