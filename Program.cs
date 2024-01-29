d4235e4e7f68dc806b79f7217785a00791cd83ec
Oskar <63914721+Skadoink@users.noreply.github.com>
2024-01-30 07:15:07 +1300

    before overhaul delegate stuff

--- Program.cs
+++ Program.cs
@@ -9,14 +9,15 @@ manager.DeviceAdded += new DeviceAddedDelegate(manager_DeviceAdded);
 /// </summary>
 void manager_DeviceAdded(NikonManager sender, NikonDevice device)
 {
-    /*Console.WriteLine("Device added: " + device.Name);
+    Console.WriteLine("Device added: " + device.Name);
     NkMAIDCapInfo[] supportedCaps = device.GetCapabilityInfo();
     // Write list of supported capabilities to the console
     foreach (NkMAIDCapInfo supportedCap in supportedCaps)
     {
         Console.WriteLine(supportedCap.GetDescription());
-    }*/
+    }
 
+    // Write battery level to the console
     try
     {
         int batteryLevel = device.GetInteger(eNkMAIDCapability.kNkMAIDCapability_BatteryLevel);
@@ -26,12 +27,47 @@ void manager_DeviceAdded(NikonManager sender, NikonDevice device)
     {
         Console.WriteLine("Error getting battery level: " + ex.Message);
     }
-
     device.ImageReady += new ImageReadyDelegate(device_ImageReady);
-    device.CapabilityValueChanged += new CapabilityChangedDelegate(device_CapabilityValueChanged);
+    device.CaptureComplete += new CaptureCompleteDelegate(onCaptureComplete);
+    try
+    {
+        // Verify ImageReady capability is supported before adding the delegate
+        if (device.IsCapabilitySupported(eNkMAIDCapability.eNkMAIDCapability_ImageReady))
+        {
+            device.ImageReady += new ImageReadyDelegate(device_ImageReady);
+        }
+        else
+        {
+            Console.WriteLine("ImageReady capability is not supported on this device.");
+        }
 
-    device.Start(eNkMAIDCapability.kNkMAIDCapability_Capture);
-//    device.Capture();
+        // Verify CaptureComplete capability is supported before adding the delegate
+        if (device.IsCapabilitySupported(eNkMAIDCapability.eNkMAIDCapability_CaptureComplete))
+        {
+            device.CaptureComplete += new CaptureCompleteDelegate(onCaptureComplete);
+        }
+        else
+        {
+            Console.WriteLine("CaptureComplete capability is not supported on this device.");
+        }
+
+        // Verify CapabilityValueChanged capability is supported before adding the delegate
+        if (device.IsCapabilitySupported(eNkMAIDCapability.eNkMAIDCapability_CapabilityValueChanged))
+        {
+            device.CapabilityValueChanged += new CapabilityChangedDelegate(device_CapabilityValueChanged);
+        }
+        else
+        {
+            Console.WriteLine("CapabilityValueChanged capability is not supported on this device.");
+        }
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine("Error adding delegates: " + ex.Message);
+    }
+
+    device.Start(eNkMAIDCapability.kNkMAIDCapability_Capture); //captur
+    device.Capture();
 
     sender.Shutdown();
 }
@@ -50,4 +86,9 @@ void device_ImageReady(NikonDevice sender, NikonImage image)
 void device_CapabilityValueChanged(NikonDevice sender, eNkMAIDCapability capability)
 {
     Console.WriteLine("Capability value changed: " );
+}
+
+void onCaptureComplete(NikonDevice sender, int data)
+{
+    Console.WriteLine("Capture complete: " + data);
 }
