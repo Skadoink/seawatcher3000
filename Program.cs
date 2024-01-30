befbf0864d6b9985b76657a79d2ccead27672d96
Oskar <63914721+Skadoink@users.noreply.github.com>
2024-01-30 16:12:36 +1300

    removed silly class stuff for now

--- Program.cs
+++ Program.cs
@@ -3,169 +3,146 @@ using System.Diagnostics;
 using System.Windows.Media.Imaging;
 using System.Windows.Threading;
 using System.IO;
-using System.ComponentModel;
 
-// View model base class
-abstract class ViewModelBase : INotifyPropertyChanged
+
+NikonManager manager = new("Type0020.md3");
+NikonDevice _device;
+DispatcherTimer _timer = new();
+_timer.Interval = TimeSpan.FromMilliseconds(100);
+manager.DeviceAdded += new DeviceAddedDelegate(manager_DeviceAdded);
+
+
+/// <summary>
+/// What to do when a device is added
+/// </summary>
+void manager_DeviceAdded(NikonManager sender, NikonDevice device)
 {
-    public event PropertyChangedEventHandler PropertyChanged;
+    _device = device;
+    _timer.Tick += new EventHandler(timerTick);
 
-    protected void OnPropertyChanged(string propertyName)
+    Trace.WriteLine("Device added: " + device.Name);
+    NkMAIDCapInfo[] supportedCaps = device.GetCapabilityInfo();
+    // Write list of supported capabilities to the console
+    foreach (NkMAIDCapInfo supportedCap in supportedCaps)
     {
-        if (PropertyChanged != null)
+        Console.WriteLine(supportedCap.GetDescription());
+    }
+
+    // Write battery level to the console
+    try
+    {
+        int batteryLevel = device.GetInteger(eNkMAIDCapability.kNkMAIDCapability_BatteryLevel);
+        Console.WriteLine("Battery level: " + batteryLevel);
+    }
+    catch (NikonException ex)
+    {
+        Console.WriteLine("Error getting battery level: " + ex.Message);
+    }
+
+
+    device.Capture();
+    StartLiveView();
+    StopLiveView();
+
+    sender.Shutdown();
+}
+
+void StartLiveView()
+{
+    try
+    {
+        NikonDevice device = _device as NikonDevice;
+        if (device != null)
         {
-            PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            device.LiveViewEnabled = true;
+            _timer.Start();
         }
+    }
+    catch (NikonException ex)
+    {
+        Console.WriteLine("Failed to start live view: " + ex.ToString());
     }
 }
 
-// View model for NikonDevice and NikonManager (classes that inherit from NikonBase)
-class ObjectModel : ViewModelBase
+void StopLiveView()
 {
-    NikonManager manager = new("Type0020.md3");
-    NikonDevice _device;
-    DispatcherTimer _timer = new();
-
-    public ObjectModel(NikonBase obj)
+    try
     {
-        _device = obj as NikonDevice;
-        _timer.Interval = TimeSpan.FromMilliseconds(100);
-        manager.DeviceAdded += new DeviceAddedDelegate(manager_DeviceAdded);
-    }
+        NikonDevice device = _device as NikonDevice;
 
-
-    /// <summary>
-    /// What to do when a device is added
-    /// </summary>
-    void manager_DeviceAdded(NikonManager sender, NikonDevice device)
-    {
-        _device = device;
-        _timer.Tick += new EventHandler(timerTick);
-
-        Trace.WriteLine("Device added: " + device.Name);
-        NkMAIDCapInfo[] supportedCaps = device.GetCapabilityInfo();
-        // Write list of supported capabilities to the console
-        foreach (NkMAIDCapInfo supportedCap in supportedCaps)
-        {
-            Console.WriteLine(supportedCap.GetDescription());
-        }
-
-        // Write battery level to the console
-        try
-        {
-            int batteryLevel = device.GetInteger(eNkMAIDCapability.kNkMAIDCapability_BatteryLevel);
-            Console.WriteLine("Battery level: " + batteryLevel);
-        }
-        catch (NikonException ex)
-        {
-            Console.WriteLine("Error getting battery level: " + ex.Message);
-        }
-
-
-        device.Capture();
-        StartLiveView();
-        StopLiveView();
-
-        sender.Shutdown();
-    }
-
-    void StartLiveView()
-    {
-        try
-        {
-            NikonDevice device = _device as NikonDevice;
-            if (device != null)
-            {
-                device.LiveViewEnabled = true;
-                _timer.Start();
-            }
-        }
-        catch (NikonException ex)
-        {
-            Console.WriteLine("Failed to start live view: " + ex.ToString());
-        }
-    }
-
-    void StopLiveView()
-    {
-        try
-        {
-            NikonDevice device = _device as NikonDevice;
-
-            if (device != null)
-            {
-                _timer.Stop();
-                device.LiveViewEnabled = false;
-            }
-        }
-        catch (NikonException ex)
-        {
-            Console.WriteLine("Failed to stop live view: " + ex.ToString());
-        }
-    }
-
-    void timerTick(object sender, EventArgs e)
-    {
-        Debug.Assert(_device != null);
-
-        NikonLiveViewImage liveViewImage = null;
-
-        try
-        {
-            liveViewImage = _device.GetLiveViewImage();
-        }
-        catch (NikonException ex)
-        {
-            Console.WriteLine("Failed to get live view image: " + ex.ToString());
-        }
-
-        if (liveViewImage == null)
+        if (device != null)
         {
             _timer.Stop();
-            return;
+            device.LiveViewEnabled = false;
         }
+    }
+    catch (NikonException ex)
+    {
+        Console.WriteLine("Failed to stop live view: " + ex.ToString());
+    }
+}
 
-        // Note: Decode the live view jpeg image on a seperate thread to keep the UI responsive
+void timerTick(object sender, EventArgs e)
+{
+    Debug.Assert(_device != null);
 
-        ThreadPool.QueueUserWorkItem(new WaitCallback((o) =>
-        {
-            Debug.Assert(liveViewImage != null);
+    NikonLiveViewImage liveViewImage = null;
 
-            JpegBitmapDecoder decoder = new JpegBitmapDecoder(
-                new MemoryStream(liveViewImage.JpegBuffer),
-                BitmapCreateOptions.None,
-                BitmapCacheOption.OnLoad);
-
-            Debug.Assert(decoder.Frames.Count > 0);
-            BitmapFrame frame = decoder.Frames[0];
-
-            Dispatcher.CurrentDispatcher.Invoke((Action)(() =>
-            {
-                //SetLiveViewImage(frame);
-            }));
-        }));
-
-        Save(liveViewImage.JpegBuffer, "liveview.jpg");
+    try
+    {
+        liveViewImage = _device.GetLiveViewImage();
+    }
+    catch (NikonException ex)
+    {
+        Console.WriteLine("Failed to get live view image: " + ex.ToString());
     }
 
-    void Save(byte[] buffer, string file)
+    if (liveViewImage == null)
     {
-        string path = Path.Combine(
-            System.Environment.GetFolderPath(Environment.SpecialFolder.MyPictures),
-            file);
+        _timer.Stop();
+        return;
+    }
 
-        Trace.WriteLine("Saving: " + path);
+    // Note: Decode the live view jpeg image on a seperate thread to keep the UI responsive
 
-        try
+    ThreadPool.QueueUserWorkItem(new WaitCallback((o) =>
+    {
+        Debug.Assert(liveViewImage != null);
+
+        JpegBitmapDecoder decoder = new JpegBitmapDecoder(
+            new MemoryStream(liveViewImage.JpegBuffer),
+            BitmapCreateOptions.None,
+            BitmapCacheOption.OnLoad);
+
+        Debug.Assert(decoder.Frames.Count > 0);
+        BitmapFrame frame = decoder.Frames[0];
+
+        Dispatcher.CurrentDispatcher.Invoke((Action)(() =>
         {
-            using (FileStream stream = new FileStream(path, FileMode.Create, FileAccess.Write))
-            {
-                stream.Write(buffer, 0, buffer.Length);
-            }
-        }
-        catch (Exception ex)
+            //SetLiveViewImage(frame);
+        }));
+    }));
+
+    Save(liveViewImage.JpegBuffer, "liveview.jpg");
+}
+
+void Save(byte[] buffer, string file)
+{
+    string path = Path.Combine(
+        System.Environment.GetFolderPath(Environment.SpecialFolder.MyPictures),
+        file);
+
+    Trace.WriteLine("Saving: " + path);
+
+    try
+    {
+        using (FileStream stream = new FileStream(path, FileMode.Create, FileAccess.Write))
         {
-            Console.WriteLine("Failed to save file: " + path + ", " + ex.Message);
+            stream.Write(buffer, 0, buffer.Length);
         }
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine("Failed to save file: " + path + ", " + ex.Message);
     }
 }
