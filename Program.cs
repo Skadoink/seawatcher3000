816398b50015c0a7b744b98c1eeba7cecd9f0737
Oskar <oskaege@gmail.com>
2024-06-08 13:49:27 +1200

    failing to get liveview on gui working

--- Program.cs
+++ Program.cs
@@ -8,15 +8,19 @@ using SixLabors.ImageSharp;
 using SixLabors.ImageSharp.PixelFormats;
 using SixLabors.ImageSharp.Processing;
 using Compunet.YoloV8.Plotting;
+using System.Windows.Data;
+using System.ComponentModel;
+using System.Collections.ObjectModel;
 
 namespace seawatcher3000
 {
-    class Seawatcher
+    class Seawatcher : ViewModelBase
     {
         public static NikonManager manager = new("Type0020.md3");
         NikonDevice _device;
         public static DispatcherTimer _timer = new();
         private YoloV8Predictor predictor;
+        Image _liveViewImage;
 
         public Seawatcher()
         {
@@ -124,6 +128,8 @@ namespace seawatcher3000
                 Mode = ResizeMode.Pad // This preserves aspect ratio and pads the image with a background color
             }));
 
+            SetLiveViewImage(image);
+
             var watch = Stopwatch.StartNew(); //start stopwatch:
             watch.Start();
             var result = predictor.Detect(image);
@@ -132,8 +138,8 @@ namespace seawatcher3000
             if (result.ToString() == "")
             {
                 Trace.WriteLine("No birds detected");
-                uint afPt = _device.GetUnsigned(eNkMAIDCapability.kNkMAIDCapability_AutoFocusPt);
-                Trace.WriteLine("Auto focus point: " + afPt);
+                //uint afPt = _device.GetUnsigned(eNkMAIDCapability.kNkMAIDCapability_AutoFocusPt);
+                //Trace.WriteLine("Auto focus point: " + afPt);
             }
             else
             {
@@ -152,9 +158,9 @@ namespace seawatcher3000
                 // Focus the camera on the target coordinates, or as close as possible given the camera's focus points. 
                 // This is going to be difficult due to the >100ms delay, so the focus could miss the bird and focus on the background instead.
                 // This would be especially bad if the camera is at ground level because it could focus on the sky, making the bird very blurry.
-                uint afPt = _device.GetUnsigned(eNkMAIDCapability.kNkMAIDCapability_AutoFocusPt);
-                Trace.WriteLine("Auto focus point: " + afPt);
-                _device.Start(eNkMAIDCapability.kNkMAIDCapability_AutoFocus);
+                //uint afPt = _device.GetUnsigned(eNkMAIDCapability.kNkMAIDCapability_AutoFocusPt);
+                //Trace.WriteLine("Auto focus point: " + afPt);
+                //_device.Start(eNkMAIDCapability.kNkMAIDCapability_AutoFocus);
 
                 // Save the image with the detection results, but we don't want to save too often, especially if there's a persistant false positive!
                 result.PlotImage(image);
@@ -182,6 +188,32 @@ namespace seawatcher3000
             catch (Exception ex)
             {
                 Console.WriteLine("Failed to save file: " + path + ", " + ex.Message);
+            }
+        }
+
+        void SetLiveViewImage(Image image)
+        {
+            _liveViewImage = image;
+            OnPropertyChanged("LiveViewImage");
+        }
+
+        public Image LiveViewImage
+        {
+            get { return _liveViewImage; }
+        }
+
+    }
+
+    // View model base class
+    abstract class ViewModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
             }
         }
     }
